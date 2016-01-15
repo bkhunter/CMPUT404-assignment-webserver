8aa1b0955f2db638a552c559c0089eae08576629
Ben Hunter <bkhunter@ualberta.ca>
2016-01-15 14:28:07 -0700

    having problems with the css tests, experimenting

--- server.py
+++ server.py
@@ -65,9 +65,11 @@ class MyWebServer1(SocketServer.BaseRequestHandler):
         path = ''
         isInPath = False
         i = 0
-        while self.data[i] != '\n':
-            message += self.data[i]
-            i+=1
+        for element in self.data:
+            if element != '\n':
+                message += element
+            else:
+                break
         for char in message:
             if char == '/':
                 isInPath = True
@@ -76,11 +78,12 @@ class MyWebServer1(SocketServer.BaseRequestHandler):
                     break
                 else:
                     path += char
-                
+                            
         return message,path
-                 
+        
     def makeResponse(self,initLine, dateLine, lenLine, mimeLine, content):
-        response = initLine + '\n' + dateLine + '\n' + lenLine + '\n' + mimeLine + '\n' + '\r\n\r\n' + content
+        response = initLine + '\r\n' + lenLine + '\r\n' + mimeLine + '\r\n\r\n' + content
+        #+ dateLine + '\n'      
         return response
 
     def notFound(self):
@@ -91,71 +94,71 @@ class MyWebServer1(SocketServer.BaseRequestHandler):
         mimeType = 'text/html; charset=utf-8'
 
         response = self.makeResponse(HTTP_Code,date,errorLength,mimeType,errorMsg)
-        print response
         return response
         
     def do_GET(self,path):
         cwd = os.getcwd()
         curDir = cwd + '/www'
         date = "Date:"+ str(datetime.datetime.now())
-        isFile = False;
+        toOpen = None
 
-        if isValidPath(path) :
+        if isValidPath(path):
             if path.endswith('.css'):
+                print('css')
                 mimeType = 'text/css; charset=utf-8'
-                isFile = True
-            elif path.endswith('.html'):
+            elif path.endswith('.html') :
                 mimeType = 'text/html; charset=utf-8'
-                isFile = True
+                toOpen = curDir + path
+            elif path.endswith('/'): # display index.html
+                mimeType = 'text/html; charset=utf-8'
+                toOpen = curDir + path + 'index.html'
+            elif path.endswith('deep'): # display deep.html
+                mimeType = 'text/html; charset=utf-8'
+                toOpen = curDir + path + '/index.html'
+
             try:
-                if isFile:
-                    if path == '/deep.css':
-                        toOpen = curDir + '/deep' + path
-                        display = open(toOpen)
-                        HTTP_Code = "HTTP/1.1 200 OK"
-                        data = display.read()
-                        length = str(len(data))
-                        display.close()
-                        response = self.makeResponse(HTTP_Code,date,length,mimeType,data)
-                        return response
-                    else:
-                        x = self.notFound()
-                        return x
+                print 'In try'
+                if path == '/deep.css':
+                    toOpen = curDir + '/deep' + path
+                    contents = open(toOpen,"r")
+                    HTTP_Code = "HTTP/1.1 200 OK"
+                    data = contents.read()
+                    length = str(len(data))
+                    contents.close()
+                    response = self.makeResponse(HTTP_Code,date,length,mimeType,data)
+                    return response
                 else:
-                    x = self.notFound()
-                    return x
-            except IOError:
-                 x = self.notFound()
-                 return x
+                    
+                    if toOpen is None:
+                        toOpen = curDir + path
+                    print toOpen
+                    contents = open(toOpen,"r")
+                    HTTP_Code = "HTTP/1.1 200 OK"
+                    data = contents.read()
+                    length = str(len(data))
+                    contents.close()
+                    response = self.makeResponse(HTTP_Code,date,length,mimeType,data)
+                    return response
+            except:
+                x = self.notFound()
+                return x
+        else:
+            x = self.notFound()
+            return x
                 
-                    # elif path.endswith('/'): # if the path ends with / display index.html
-                    #     display = open(toOpen+'index.html')
-                    #     self.send_response(200)
-                    #     self.send_header('Content-type', 'text/html')
-                    #     self.end_headers()
-                    #     self.wfile.write(display.read())
-                    #     display.close()
-                    # elif path.endswith('deep'): # corner case
-                    #     display = open(toOpen+'/index.html')
-                    #     self.send_response(200)
-                    #     self.send_header('Content-type', 'text/html')
-                    #     self.end_headers()
-                    #     self.wfile.write(display.read())
-                    #     display.close()
-        
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        print "---------"
         message,path = self.parseRequest()
+        print path
         if message[:3] == 'GET':
             print message
             print path
             response = self.do_GET(path)
         else:
             response = self.notFound()
-            
-        good_response = "HTTP/1.0 200 OK\nContent-Type:text/html"
+
+        print response
         self.request.sendall(response)
         
         #print "{} wrote:".format(self.client_address[0])
@@ -163,60 +166,6 @@ class MyWebServer1(SocketServer.BaseRequestHandler):
         #self.request.sendall(http_response)
         #self.send_response(200)
 
-    
-
-class MyWebServer2(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        cwd = os.getcwd()
-        curDir = cwd + '/www'
-        mimetype = ''
-        isFile = False;
-        path = self.path
-
-        if isValidPath(path) :
-            if path.endswith('.css'):
-                mimetype = 'text/css'
-                isFile = True
-            elif path.endswith('.html'):
-                mimetype = 'text/html'
-                isFile = True
-                toOpen = curDir+path
-            try:
-                if isFile:
-                    if path == '/deep.css':
-                        toOpen = curDir + '/deep' + path
-                        display = open(toOpen)
-                        self.send_response(200)
-                        self.send_header('Content-type', mimetype)
-                        self.end_headers()
-                        self.wfile.write(display.read())
-                        display.close()
-                    elif path.endswith('/'): # if the path ends with / display index.html
-                        display = open(toOpen+'index.html')
-                        self.send_response(200)
-                        self.send_header('Content-type', 'text/html')
-                        self.end_headers()
-                        self.wfile.write(display.read())
-                        display.close()
-                    elif path.endswith('deep'): # corner case
-                        display = open(toOpen+'/index.html')
-                        self.send_response(200)
-                        self.send_header('Content-type', 'text/html')
-                        self.end_headers()
-                        self.wfile.write(display.read())
-                        display.close()
-            except IOError:
-                self.send_error(404)
-        else:
-            self.send_error(404)
-            # self.send_response(404)
-            # self.send_header("Content-type", "text/html")
-            # self.end_headers()
-            # self.wfile.write("<html><head><title> 404 :( .</title></head>")
-            # self.wfile.write("<body><p>Error 404 File Not Found.</p>")
-            # self.wfile.write("</body></html>")
-                            
 if __name__ == "__main__":
     HOST = "localhost" 
     PORT =  8080
@@ -230,3 +179,34 @@ if __name__ == "__main__":
     print 'Serving HTTP on port %s ...' % PORT
     server.serve_forever()
 
+
+      # elif path.endswith('/'): # if the path ends with / display index.html
+      #               print('////!!!!////')
+      #               contents = open(toOpen+'index.html')
+      #               HTTP_Code = "HTTP/1.1 200 OK"
+      #               mimeType = 'text/html; charset=utf-8'
+      #               data = contents.read()
+      #               length = str(len(data))
+      #               contents.close()
+      #               response = self.makeResponse(HTTP_Code,date,length,mimeType,data)
+      #               return response
+      #           else:
+      #               x = self.notFound()
+      #               return x
+
+
+
+   # elif path.endswith('/'): # if the path ends with / display index.html
+   #     display = open(toOpen+'index.html')
+   #     self.send_response(200)
+   #     self.send_header('Content-type', 'text/html')
+   #     self.end_headers()
+   #     self.wfile.write(display.read())
+   #     display.close()
+   # elif path.endswith('deep'): # corner case
+   #     display = open(toOpen+'/index.html')
+   #     self.send_response(200)
+   #     self.send_header('Content-type', 'text/html')
+   #     self.end_headers()
+   #     self.wfile.write(display.read())
+   #     display.close()
